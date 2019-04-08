e1cb150a4cfb8d0d4aba83b5d5c12550b21b05a8
thanachai_swa <thanachai.swa@ascendcorp.com>
2019-04-08 17:05:34 +0700

    add response

---
+++ src/main/java/com/gamitology/kevent/kevent/dto/response/EventArtistResponse.java
@@ -0,0 +1,11 @@
+package com.gamitology.kevent.kevent.dto.response;
+
+import lombok.Data;
+
+@Data
+public class EventArtistResponse {
+    private int artistId;
+    private String name;
+    private String detail;
+    private String note;
+}
